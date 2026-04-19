36459ec441dd12247cb9b09a81883d05d0b2b91e
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-19 03:52:29 +0200

    add all data api in json file this data displayed on SPU linux app and test it by using pyhon

--- code_of_project/Data_Base_Python/SPU_main_Data_handlig.py
+++ code_of_project/Data_Base_Python/SPU_main_Data_handlig.py
@@ -1,11 +1,24 @@
-
+# Arduino (Sensors)
+#         ↓
+#    Serial / WiFi
+#         ↓
+# Python (Processing + AI Model)
+#         ↓
+#    JSON Output
+#         ↓
+# Flutter App (Dashboard)
 from Json_handling_data import Data
 import os
-
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(base_dir, "SPU_Data_between_app_and_python_Rassbiary_pi.json")
 
 Data_from_app = Data(file_path)
 
-print(Data_from_app.Read_Data())
+data = Data_from_app.Read_Data()
+motor_current = data["Actuators"]["Motor"]["Sensors"]["Current"]
+motor_status = data["Actuators"]["Motor"]["status"]
+print(f"value ->  {type(motor_current)}   ->   {motor_current}")
+print(f"value ->  {type(motor_status)}   ->   {motor_status}")
+
+
