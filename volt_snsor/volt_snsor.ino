64b9d6355fbae415406bda5e09237b2563a8ea70
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-20 12:55:55 +0200

    add folders to teast sensors and add realesa folder on it .exe test app

---
+++ volt_snsor/volt_snsor.ino
@@ -0,0 +1,14 @@
+
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int raw = analogRead(A0);
+
+  float voltage = (raw * 5.0) / 1023.0;
+
+  Serial.println(voltage);
+
+  delay(500);
+}
