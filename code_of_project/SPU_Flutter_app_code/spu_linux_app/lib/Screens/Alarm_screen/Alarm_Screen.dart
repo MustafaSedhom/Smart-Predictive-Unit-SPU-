7972ef9ff56c3457ab10cec3345b17b9e3182ae8
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-21 07:02:28 +0200

    add on flutter app screen alarm and add on it list view of container to show it to test

--- code_of_project/SPU_Flutter_app_code/spu_linux_app/lib/Screens/Alarm_screen/Alarm_Screen.dart
+++ code_of_project/SPU_Flutter_app_code/spu_linux_app/lib/Screens/Alarm_screen/Alarm_Screen.dart
@@ -1,23 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
-import 'package:spu_linux_app/colors/App_colors.dart';
 
 class AlarmScreen extends StatelessWidget {
-  const AlarmScreen({super.key});
-
+  AlarmScreen({super.key});
+  final List<Map<String, dynamic>> items = [
+    {"title": "Motor", "value": "Running", "icon": Icons.settings},
+    {"title": "Temperature", "value": "32°C", "icon": Icons.thermostat},
+    {"title": "Vibration", "value": "1.2 mm/s", "icon": Icons.graphic_eq},
+    {"title": "Motor", "value": "Running", "icon": Icons.settings},
+    {"title": "Temperature", "value": "32°C", "icon": Icons.thermostat},
+    {"title": "Vibration", "value": "1.2 mm/s", "icon": Icons.graphic_eq},
+    {"title": "Motor", "value": "Running", "icon": Icons.settings},
+    {"title": "Temperature", "value": "32°C", "icon": Icons.thermostat},
+    {"title": "Vibration", "value": "1.2 mm/s", "icon": Icons.graphic_eq},
+    {"title": "Current", "value": "1.4 A", "icon": Icons.bolt},
+    {"title": "Current", "value": "1.4 A", "icon": Icons.bolt},
+    {"title": "Current", "value": "1.4 A", "icon": Icons.bolt},
+    {"title": "Current", "value": "1.4 A", "icon": Icons.bolt},
+  ];
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Gap(20),
-          Text(
-            "data",
-            style: TextStyle(fontSize: 30, color: AppColors.Drawer_text_color),
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          return Container(
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade900,
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 6,
+                  offset: Offset(2, 2),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Icon(item["icon"], size: 30, color: Colors.green),
+                const SizedBox(width: 15),
+
+                /// TEXTS
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item["title"],
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        item["value"],
+                        style: TextStyle(color: Colors.white70, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+
+                /// STATUS ICON
+                Icon(Icons.circle, color: Colors.green, size: 12),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
