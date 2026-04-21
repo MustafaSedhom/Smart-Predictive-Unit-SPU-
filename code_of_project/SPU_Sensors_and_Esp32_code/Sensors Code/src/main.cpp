a409616a17d9cea5b4203485ce76b2d8bb28e003
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-21 02:32:14 +0200

    init platfoem to program sensors and esp32

--- code_of_project/SPU_Sensors_and_Esp32_code/Sensors Code/src/main.cpp
+++ code_of_project/SPU_Sensors_and_Esp32_code/Sensors Code/src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
+#include "ArduinoJson.h"
+void setup() 
+{
+  Serial.begin(9600);
 
-// put function declarations here:
-int myFunction(int, int);
+  JsonDocument doc;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  doc["temperature"] = 32.5;
+  doc["vibration"] = 1.2;
+  doc["current"] = 1.4;
+
+  serializeJson(doc, Serial);
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop() 
+{
 }
