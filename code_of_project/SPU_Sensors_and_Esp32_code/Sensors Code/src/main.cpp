eff84e03941a9269c01a7e8cc917ce5b2e075623
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-21 04:42:54 +0200

    handling sensors data as folders in main ESP32 program to use it and can add and remove sensors and functions easly

--- code_of_project/SPU_Sensors_and_Esp32_code/Sensors Code/src/main.cpp
+++ code_of_project/SPU_Sensors_and_Esp32_code/Sensors Code/src/main.cpp
@@ -1,19 +1,29 @@
+//-----------------------------------------------------------
+// import librarys
 #include <Arduino.h>
 #include "ArduinoJson.h"
+//-----------------------------------------------------------
+// defines 
+
+//-----------------------------------------------------------
+// Global Variables
+
+//-----------------------------------------------------------
+// init program
 void setup() 
 {
-  Serial.begin(9600);
-
-  JsonDocument doc;
-
-  doc["temperature"] = 32.5;
-  doc["vibration"] = 1.2;
-  doc["current"] = 1.4;
-
-  serializeJson(doc, Serial);
-  Serial.println();
+    Serial.begin(9600);
+    JsonDocument doc;
+    doc["temperature"] = 32.5;
+    doc["vibration"] = 1.2;
+    doc["current"] = 1.4;
+    serializeJson(doc, Serial);
+    Serial.println();
 }
-
+//-----------------------------------------------------------
+// program
 void loop() 
 {
+    
 }
+//-----------------------------------------------------------
