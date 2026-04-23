4104ba1575f5e371d05cd5da02407c834885df68
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-23 12:44:07 +0200

    add pading to cintainer and

--- code_of_project/SPU_Flutter_app_code/spu_linux_app/lib/Screens/Details_screen/Details_screen.dart
+++ code_of_project/SPU_Flutter_app_code/spu_linux_app/lib/Screens/Details_screen/Details_screen.dart
@@ -3,13 +3,13 @@ import 'package:gap/gap.dart';
 import 'package:spu_linux_app/Screens/Details_screen/widgets/Sensor_details_container.dart';
 
 class DetailsScreen extends StatefulWidget {
-  const DetailsScreen({super.key});
-
+  DetailsScreen({super.key});
   @override
   State<DetailsScreen> createState() => _DetailsScreenState();
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -38,7 +38,7 @@ class _DetailsScreenState extends State<DetailsScreen> {
             child: Padding(
               padding: const EdgeInsets.all(12),
               child: ListView.builder(
-                itemCount: 5,
+                itemCount: 20,
                 itemBuilder: (context, index) {
                   return SensorDetailsContainer(txt: "$index");
                 },
