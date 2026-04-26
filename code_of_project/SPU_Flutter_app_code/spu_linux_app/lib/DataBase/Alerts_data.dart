89946de709b6a3ff7fec9259bbef25ee0605afee
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-26 06:49:38 +0300

    add file in data base folder in app to get alerts in and read it from Json file

---
+++ code_of_project/SPU_Flutter_app_code/spu_linux_app/lib/DataBase/Alerts_data.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: non_constant_identifier_names
+// "Alerts":
+// {
+// "List_item_count": 30,
+// "Alert_List":[
+//             {
+//               "id": 1,
+//               "device":"Motor",
+//               "timestamp": "2006-02-22T11:15:04",
+//               "type": "temperature",
+//               "message": "Motor overheating",
+//               "level": "high",
+//               "value": 80
+//             },
+//             {
+//               "id": 2,
+//               "device":"Motor",
+//               "timestamp": "2006-02-22T11:20:10",
+//               "type": "temperature",
+//               "message": "Temperature still high",
+//               "level": "high",
+//               "value": 82
+//             }
+//         ]
+// },
+import 'dart:convert';
+import 'dart:io';
+import 'package:spu_linux_app/DataBase/json_file_path.dart';
+
+class AlertsData {
+  final int count;
+  final List<AlertsListData> alerts;
+
+  AlertsData({required this.count, required this.alerts});
+
+  factory AlertsData.fromJson(Map<String, dynamic> json) {
+    final alertsJson = json["Alerts"] as Map<String, dynamic>? ?? {};
+    final list = alertsJson['Alert_List'] as List<dynamic>? ?? [];
+
+    final alertsList = list.map((e) => AlertsListData.fromJson(e)).toList();
+
+    return AlertsData(
+      count:
+          (alertsJson['List_item_count'] as num?)?.toInt() ?? alertsList.length,
+      alerts: alertsList,
+    );
+  }
+}
+
+class AlertsListData {
+  final int id;
+  final String device;
+  final String type;
+  final String message;
+  final String level;
+  final double value;
+  final DateTime timestamp;
+
+  AlertsListData({
+    required this.id,
+    required this.device,
+    required this.type,
+    required this.message,
+    required this.level,
+    required this.value,
+    required this.timestamp,
+  });
+
+  factory AlertsListData.fromJson(Map<String, dynamic> json) {
+    return AlertsListData(
+      id: (json['id'] as num).toInt(),
+      device: json['device'] ?? "",
+      type: json['type'] ?? "",
+      message: json['message'] ?? "",
+      level: json['level'] ?? "low",
+      value: (json['value'] as num).toDouble(),
+      timestamp: DateTime.tryParse(json['timestamp'] ?? "") ?? DateTime.now(),
+    );
+  }
+}
+
+AlertsListData none_alert_list_data = AlertsListData(
+  id: 0,
+  device: "none",
+  type: "None",
+  message: "msg",
+  level: "Low",
+  value: 0,
+  timestamp: DateTime.now(),
+);
+AlertsData none_alert_data = AlertsData(count: 0, alerts: []);
+List<AlertsListData> parseAlertsList(List<dynamic> list) {
+  return list.map((e) => AlertsListData.fromJson(e)).toList();
+}
+
+// load file
+Future<AlertsData> loadAlertsDataFromFile() async {
+  try {
+    final path = JsonFilePath.path;
+
+    if (path == null || path.isEmpty) {
+      return none_alert_data;
+    }
+
+    final file = File(path);
+
+    if (!await file.exists()) return none_alert_data;
+
+    final data = await file.readAsString();
+
+    if (data.isEmpty) return none_alert_data;
+
+    final jsonResult = jsonDecode(data);
+
+    return AlertsData.fromJson(jsonResult);
+  } catch (e) {
+    return none_alert_data;
+  }
+}
