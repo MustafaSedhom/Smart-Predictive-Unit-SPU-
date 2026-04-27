aef1f22411ab887e3018524669aae8bfa72d7491
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-27 08:19:15 +0300

    cxhnage linux app name and icon

--- code_of_project/SPU_Flutter_app_code/spu_linux_app/linux/runner/my_application.cc
+++ code_of_project/SPU_Flutter_app_code/spu_linux_app/linux/runner/my_application.cc
@@ -32,6 +32,8 @@ static void my_application_activate(GApplication* application) {
   // in case the window manager does more exotic layout, e.g. tiling.
   // If running on Wayland assume the header bar will work (may need changing
   // if future cases occur).
+    gtk_window_set_icon_from_file(GTK_WINDOW(window), "./flutter/resources/app_icon.ico", NULL);
+
   gboolean use_header_bar = TRUE;
 #ifdef GDK_WINDOWING_X11
   GdkScreen* screen = gtk_window_get_screen(window);
@@ -45,11 +47,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "spu_linux_app");
+    gtk_header_bar_set_title(header_bar, "SPU");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "spu_linux_app");
+    gtk_window_set_title(window, "SPU");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
