90409a6860368aa1537c43097fbb8046ce665038
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-04-28 16:53:41 +0300

    add folders to recieve data from uart and add folder for processing data and convert it from string to proprties in class to acess it from objects

--- code_of_project/Rrocess_Data_From_Slave_Board/All_Sensor_Data_After_Processing/All_Sensor_Data_After_Processing.py
+++ code_of_project/Rrocess_Data_From_Slave_Board/All_Sensor_Data_After_Processing/All_Sensor_Data_After_Processing.py
@@ -1,16 +1,18 @@
-
-
-import json
 import json
 
 from Belt_Data.Belt_Data import BeltDriver
 from Motor_Data.Motor_Data import Motor
 from Pump_Data.Pump_Data import Pump
 
+
 class All_Sensor_Data_After_Receiving:
     def __init__(self, Data_var: str):
-        data = json.loads(Data_var)
 
-        self.motor = Motor(data["Motor"])
-        self.pump = Pump(data["Pump"])
-        self.belt = BeltDriver(data["Belt_Driver"])
+        try:
+            data = json.loads(Data_var)
+        except json.JSONDecodeError:
+            raise ValueError("Invalid JSON string received from UART")
+
+        self.motor = Motor(data.get("Motor", {}))
+        self.pump = Pump(data.get("Pump", {}))
+        self.belt = BeltDriver(data.get("Belt_Driver", {}))
