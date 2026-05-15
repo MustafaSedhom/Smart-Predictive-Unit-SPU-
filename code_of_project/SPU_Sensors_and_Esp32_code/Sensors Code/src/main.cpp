19027f9595a0ffc8dcd8d327ec0d85fe841d4097
Eng.Mostafa Sedhom <elmohandes24680@gmail.com>
2026-05-15 09:56:52 +0300

    in esp32 code check in loop if temp > 100 then temp =0 for motor and pump becuse it make crash on app

--- code_of_project/SPU_Sensors_and_Esp32_code/Sensors Code/src/main.cpp
+++ code_of_project/SPU_Sensors_and_Esp32_code/Sensors Code/src/main.cpp
@@ -5,7 +5,7 @@
 #include "Handling_Communction_Data/Json_Data.h"
 //-----------------------------------------------------------
 // defines 
-
+#define delay_time 1000
 //-----------------------------------------------------------
 // Global Variables
 unsigned long lastSend;
@@ -18,21 +18,27 @@ Pump pump , last_pump;
 void setup() 
 {
     Serial.begin(115200);
-    motor = Motor(45,2.1,Phases(221.3,220.1,33),Phases(5.6,2.1,1.0));
-    pump = Pump(18.3,22.4,78);
-    belt = Belt(5678,3.5,222);
+    motor = Motor(98,2.1,Phases(221.3,220.1,33),Phases(7.7,2.1,1.0));
+    pump = Pump(18.3,22.4,55);
+    belt = Belt(1122,3.5,555);
     Json.updateMotor(motor);
     Json.updatePump(pump);
     Json.updateBelt(belt);
+    lastSend = millis();
 }
 //-----------------------------------------------------------
 // program
 void loop()  
 {   
-    if (millis() - lastSend > 1000)
+    // check if it's time to send data
+    if (millis() - lastSend >= delay_time)
     {
+        // check temp is not above 100 to avoid overflow
+        if (motor.Temperature > 100) motor.Temperature = 0;
+        if (pump.Temperature > 100) pump.Temperature = 0;
         if(motor != last_motor || pump != last_pump || belt != last_belt)
         {
+            Json.updateAll(motor, belt, pump);
             Serial.println(Json.get_Json_formate()); 
             last_motor = motor;
             last_pump = pump;
